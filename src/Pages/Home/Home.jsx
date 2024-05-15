6128a3a7fdc0fea19002806367f8e7832c2d3e6a
V SHANMUKHA RAJU <rajunarayana1248@gmail.com>
2024-05-15 21:33:48 +0530

    added env file

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,9 @@
-
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import Navbar from "../../Components/Navbar/Navbar";
-import CurrentWheather from '../../Components/CurrentWheather/CurrentWheather';
-import Forecast from '../../Components/Forecast/Forecast';
+import CurrentWheather from "../../Components/CurrentWheather/CurrentWheather";
+import Forecast from "../../Components/Forecast/Forecast";
 
-import './Home.css'
+import "./Home.css";
 
 const Home = () => {
   const [currentwheather, setCurrentWheather] = useState(null);
@@ -12,34 +11,38 @@ const Home = () => {
 
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
+    const api_url = process.env.VITE_REACT_APP_API_URL;
+    const api_key = process.env.VITE_REACT_APP_API_KEY;
 
-    const currentwheatherfetch = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7d0458209e9454e04e9e89ce388feaa6&units=metric`)
-    const forecastfetch = fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=7d0458209e9454e04e9e89ce388feaa6&units=metric`)
+    const currentwheatherfetch = fetch(
+      `${api_url}/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`
+    );
+    const forecastfetch = fetch(
+      `${api_url}/forecast?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`
+    );
 
     Promise.all([currentwheatherfetch, forecastfetch])
-    .then(async (response) => {
-      const wheatherResopnse = await response[0].json();
-      const forecastResopnse = await response[1].json();
+      .then(async (response) => {
+        const wheatherResopnse = await response[0].json();
+        const forecastResopnse = await response[1].json();
 
-      setCurrentWheather({city: searchData.label, ...wheatherResopnse})
-      setCurrentForecast({city: searchData.label, ...forecastResopnse})
-      
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  }
+        setCurrentWheather({ city: searchData.label, ...wheatherResopnse });
+        setCurrentForecast({ city: searchData.label, ...forecastResopnse });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   console.log(currentwheather);
-  console.log(currentForecast)
-
+  console.log(currentForecast);
 
   return (
-    <div className='home'>
-        <Navbar onSearchChange={handleOnSearchChange} />
-        {currentwheather && <CurrentWheather data={currentwheather} />}
-        {currentForecast && <Forecast  data={currentForecast}/>}
+    <div className="home">
+      <Navbar onSearchChange={handleOnSearchChange} />
+      {currentwheather && <CurrentWheather data={currentwheather} />}
+      {currentForecast && <Forecast data={currentForecast} />}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
