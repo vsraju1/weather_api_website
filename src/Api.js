fd975ec7cc39cd4bb9b162ce562a88dc94411e6f
V SHANMUKHA RAJU <rajunarayana1248@gmail.com>
2024-05-15 21:42:50 +0530

    added env files key

--- src/Api.js
+++ src/Api.js
@@ -2,7 +2,7 @@ export const geoApiUrl = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'ab0ea630c3msh45fc02e93e5855fp1755f3jsn0021c60d104c',
+		'X-RapidAPI-Key': import.meta.env.VITE_REACT_CITIES_KEY,
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
